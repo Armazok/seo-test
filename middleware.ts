d7f017622f376bc24e7adab637f962bbee22ea69
Armazok <nikolai.borsin@mail.ru>
2026-01-08 23:36:56 +0300

    add middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,49 +7,25 @@ export function middleware(req: NextRequest) {
     ).toString('base64');
 
     const csp = `
-  default-src 'self';
-
-  script-src 
-    'self' 
-    'nonce-${nonce}' 
-    'strict-dynamic' 
-    https://www.googletagmanager.com;
-
-  script-src-elem 
-    'self' 
-    'nonce-${nonce}' 
-    https://www.googletagmanager.com;
-
-  script-src-attr 'none';
-
-  style-src 
-    'self' 
-    https://fonts.googleapis.com;
-
-  style-src-elem 
-    'self' 
-    https://fonts.googleapis.com;
-
-  style-src-attr 'none';          # закрыто для inline
-
-  font-src 'self' https://fonts.gstatic.com;
-
-  img-src 'self' data:;
-
-  connect-src 'self';
-
-  frame-src 'none';               # запрещены все iframe
-  frame-ancestors 'none';
-
-  form-action 'self';
-  base-uri 'none';
-  object-src 'none';
-
-  navigate-to 'self';             # запрещён javascript: и внешние навигации
-
-  require-trusted-types-for 'script';
-  trusted-types nextjs next-router;
-`.replace(/\s{2,}/g, ' ').trim();
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com;
+    script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
+    script-src-attr 'none';
+    style-src 'self' https://fonts.googleapis.com;
+    style-src-elem 'self' https://fonts.googleapis.com;
+    style-src-attr 'unsafe-inline';   
+    font-src 'self' https://fonts.gstatic.com;
+    img-src 'self' data:;
+    connect-src 'self';
+    frame-src https://www.googletagmanager.com;
+    form-action 'self';
+    frame-ancestors 'none';
+    base-uri 'none';
+    object-src 'none';
+    navigate-to 'self';
+    require-trusted-types-for 'script';
+    trusted-types nextjs next-router;
+  `.replace(/\s{2,}/g, ' ').trim();
 
     const res = NextResponse.next();
 
