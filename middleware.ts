1fad7de57e5af647c02303e67137dc377cfd6b01
Armazok <nikolai.borsin@mail.ru>
2026-01-09 00:00:17 +0300

    add middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -25,10 +25,11 @@ export function middleware(req: NextRequest) {
   navigate-to 'self';        /* запрет навигации на javascript: URL */
   require-trusted-types-for 'script';
   trusted-types nextjs next-router;
-  block-all-mixed-content;  /* для безопасности iframe/css */
 `.replace(/\s{2,}/g, ' ').trim();
 // sandbox allow-scripts allow-same-origin; /* если нужно iframe */
 // unsafe-eval 'none';       /* блокирует eval() */
+// block-all-mixed-content;  /* для безопасности iframe/css */
+
 
     const res = NextResponse.next();
 
