0983db5022c964d815377750687c9c91c12f95b0
Armazok <nikolai.borsin@mail.ru>
2026-01-08 23:49:04 +0300

    add middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,25 +7,29 @@ export function middleware(req: NextRequest) {
     ).toString('base64');
 
     const csp = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com;
-    script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
-    script-src-attr 'none';
-    style-src 'self' https://fonts.googleapis.com;
-    style-src-elem 'self' https://fonts.googleapis.com;
-    style-src-attr 'none';   
-    font-src 'self' https://fonts.gstatic.com;
-    img-src 'self' data:;
-    connect-src 'self';
-    frame-src https://www.googletagmanager.com;
-    form-action 'self';
-    frame-ancestors 'none';
-    base-uri 'none';
-    object-src 'none';
-    navigate-to 'self';
-    require-trusted-types-for 'script';
-    trusted-types nextjs next-router;
-  `.replace(/\s{2,}/g, ' ').trim();
+  default-src 'self';
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com;
+  script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
+  script-src-attr 'none';
+  style-src 'self' https://fonts.googleapis.com;
+  style-src-elem 'self' https://fonts.googleapis.com;
+  style-src-attr 'none';
+  font-src 'self' https://fonts.gstatic.com;
+  img-src 'self' data:;
+  connect-src 'self';
+  frame-src https://www.googletagmanager.com;
+  form-action 'self';
+  frame-ancestors 'none';
+  base-uri 'none';
+  object-src 'none';
+  navigate-to 'self';        /* запрет навигации на javascript: URL */
+  require-trusted-types-for 'script';
+  trusted-types nextjs next-router;
+  unsafe-eval 'none';       /* блокирует eval() */
+  block-all-mixed-content;  /* для безопасности iframe/css */
+  sandbox allow-scripts allow-same-origin; /* если нужно iframe */
+`.replace(/\s{2,}/g, ' ').trim();
+
 
     const res = NextResponse.next();
 
