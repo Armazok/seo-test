264a9fcb78dae267598cede704ff782d66ca5a50
Armazok <nikolai.borsin@mail.ru>
2026-01-09 11:24:45 +0300

    csp

--- proxy.ts
+++ proxy.ts
@@ -6,22 +6,22 @@ export function proxy(req: NextRequest) {
     const nonce = crypto.randomBytes(16).toString('base64');
 
     const csp = `
-default-src 'self';
-script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com 'strict-dynamic';
-script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
-script-src-attr 'none';
-style-src 'self' https://fonts.googleapis.com;
-style-src-elem 'self' https://fonts.googleapis.com;
-style-src-attr 'unsafe-inline';
-font-src 'self' https://fonts.gstatic.com;
-img-src 'self' data:;
-connect-src 'self' ws://localhost:3000;
-frame-src 'self' https://www.googletagmanager.com;
-form-action 'self';
-frame-ancestors 'none';
-block-all-mixed-content;
-upgrade-insecure-requests;
-`;
+            default-src 'self';
+            script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com 'strict-dynamic';
+            script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com;
+            script-src-attr 'none';
+            style-src 'self' https://fonts.googleapis.com;
+            style-src-elem 'self' https://fonts.googleapis.com;
+            style-src-attr 'unsafe-inline';
+            font-src 'self' https://fonts.gstatic.com;
+            img-src 'self' data:;
+            connect-src 'self' ws://localhost:3000;
+            frame-src 'self' https://www.googletagmanager.com;
+            form-action 'self';
+            frame-ancestors 'none';
+            block-all-mixed-content;
+            upgrade-insecure-requests;
+    `;
 
     const res = NextResponse.next();
 
