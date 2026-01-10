32f3e027a78ee2fcf5368cb492e2de12d4eb34fe
Armazok <nikolai.borsin@mail.ru>
2026-01-10 14:54:07 +0300

    микрозаметки

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -17,11 +17,38 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
     const products = await getAllProducts({})
 
+    const jsonLd = {
+        '@context': 'https://schema.org',
+        '@type': 'CollectionPage',
+        'name': 'Наши продукты',
+        'mainEntity': {
+            '@type': 'ItemList',
+            'numberOfItems': products.total,
+            'itemListElement': products.products.map((p, index) => ({
+                '@type': 'ListItem',
+                'position': index + 1,
+                'item': {
+                    '@type': 'Product',
+                    'name': p.title,
+                    'url': `https://your-site.com/products/${p.id}`,
+                }
+            }))
+        }
+    }
     return (
-        <section role={'main'} aria-label={'Главная страница продуктов'}>
-            <h1 aria-label={'Наши продукты для слабовидящих людей'}>Наши продукты</h1>
-            <ProductsList products={products}/>
-        </section>
+        <>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(jsonLd),
+                }}
+            />
+
+            <section role={'main'} aria-label={'Главная страница продуктов'}>
+                <h1 aria-label={'Наши продукты для слабовидящих людей'}>Наши продукты</h1>
+                <ProductsList products={products}/>
+            </section>
+        </>
     );
 };
 
